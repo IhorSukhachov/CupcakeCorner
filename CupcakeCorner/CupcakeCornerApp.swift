c19c32a0824ccea9e56621cac12be9c953d17d2b
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-01 13:29:55 +0200

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Ihor Sukhachov on 01.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
