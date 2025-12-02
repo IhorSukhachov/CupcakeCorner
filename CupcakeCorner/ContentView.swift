21d76674616e4f93d1ea646145ff373ae568f7af
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-02 14:01:30 +0200

    preparing customizable haptic

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -4,18 +4,38 @@
 //
 //  Created by Ihor Sukhachov on 01.12.2025.
 //
-
+import CoreHaptics
 import SwiftUI
 
 
 struct ContentView: View {
     @State private var counter: Int = 0
+    @State private var engine: CHHapticEngine?
     
     var body: some View {
         Button("tap count \(counter)") {
              counter += 1
         }
-        .sensoryFeedback(.increase, trigger: counter)
+       // .sensoryFeedback(.increase, trigger: counter)
+    }
+    func prepereHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            return
+        }
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was en error initializing the haptics engine \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            return
+        }
+        var events = [CHHapticEvent]()
+        
     }
 }
 #Preview {
