98eab01a3918eca8c6ad276ebb1eedc94e745b90
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-02 14:33:19 +0200

    describing our order class

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Red Velvet", "Chocolate", "Carrot Cake "]
+    var type = 0
+    var quantity = 1
+    var specialRequestsEnabled: Bool = false
+    var extraFrosting: Bool = false
+    var addSprinkles: Bool = false
+}
+ 
