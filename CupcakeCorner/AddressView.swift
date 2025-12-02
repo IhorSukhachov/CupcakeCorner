9b3ef474f461cdfa52ba3243725a26e8cf278306
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-02 15:01:09 +0200

    adding a placeholder for next screen

---
+++ CupcakeCorner/AddressView.swift
@@ -0,0 +1,18 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Ihor Sukhachov on 02.12.2025.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddressView()
+}
