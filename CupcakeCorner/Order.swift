95e545551dd9a2db391f5e1498bf2c497f0ab73f
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-04 12:21:24 +0200

    creating a coding keys enum

--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -9,6 +9,18 @@ import Foundation
 
 @Observable
 class Order: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestsEnabled = "specialRequestsEnabled"
+        case _extraFrosting = "extraFrosting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _city = "city"
+        case _streetAdress = "streetAddress"
+        case _zipCode = "zipCode"
+    }
+    
     static let types = ["Vanilla", "Strawberry", "Red Velvet", "Chocolate", "Carrot Cake "]
     var type = 0
     var quantity = 3
