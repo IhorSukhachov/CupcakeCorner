5b55582512677c24d568441851bb602f7d26243f
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-04 09:30:39 +0200

    new checkout view

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Ihor Sukhachov on 04.12.2025.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckoutView()
+}
