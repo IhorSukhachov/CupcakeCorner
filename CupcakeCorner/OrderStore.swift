ac78718f7a9645ff6790333f0d1743bb7217c59d
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-05 13:45:57 +0200

    storing user defaults

--- CupcakeCorner/OrderStore.swift
+++ CupcakeCorner/OrderStore.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import Observation
+
+@Observable
+class OrderStore {
+    private let saveKey = "SavedOrder"
+
+    var order: Order {
+        didSet {
+            save()
+        }
+    }
+
+    init() {
+        if let data = UserDefaults.standard.data(forKey: saveKey),
+            let decoded = try? JSONDecoder().decode(Order.self, from: data) {
+            self.order = decoded
+        } else {
+            self.order = Order()
+        }
+    }
+
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(order) {
+            UserDefaults.standard.set(encoded, forKey: saveKey)
+        }
+    }
+}
